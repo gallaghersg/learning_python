0b6b25b43522a612d2916410e26872e0cc3f44f2
gallaghersg <s.gaggler@gmail.com>
2015-01-06 19:41:27 -0800

    first exercise, incomplete

---
+++ break_me.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import math
+
+
+def type():
+	print math.sin('boobies')
+	
+
+def name():
+	print wrong	
+
+def syntax():
+	print ('hey! )
+
+def attribute():
+	#stumped so far. wish i had the lecture!
+
